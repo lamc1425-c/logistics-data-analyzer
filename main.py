73a7e44c5b75d93ccfc3b932eebd37d0bb07ef31
lamc1425-c <aguero.ch88@gmail.com>
2026-08-26 13:07:36 +0200

    Add initial data inspection

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-# Importamos pandas y le asignamos un alias "pd"
+# 1. Import pandas and assing it the alias "pd"
 import pandas as pd
 
-# En la variable df se guardará la lectura del archivo o ruta
+# 2. Load the dataset using a relative path
 df = pd.read_csv("data/orders.csv")
 
+# 3. Display the first few rows
 print(df.head())
+
+# 4. Display the total numbers of rows and columns
+print(df.shape)
+
+# 5. Display the names of all columns
+print(df.columns)
+
+# 6. Display the data typ of each column
+print(df.dtypes)
+
+# 7. Count total missing (empty) values per column
+print(df.isnull().sum())
