f61aadb8adfb5db476c1c3491ea3068a96ac4bb5
lamc1425-c <aguero.ch88@gmail.com>
2026-08-27 08:19:10 +0200

    Add Python string methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 1. Import pandas and assing it the alias "pd"
+# 1. Import pandas and assign it the alias "pd"
 import pandas as pd
 
 # 2. Load the dataset using a relative path
@@ -13,8 +13,17 @@ print(df.shape)
 # 5. Display the names of all columns
 print(df.columns)
 
-# 6. Display the data typ of each column
+# 6. Display the data type of each column
 print(df.dtypes)
 
 # 7. Count total missing (empty) values per column
 print(df.isnull().sum())
+
+
+df["city"] = df["city"].str.title()
+
+print(df["city"])
+
+df["status"] = df["status"].str.strip().str.title()
+
+print(df["status"])
